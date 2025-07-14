d52f30ca82fa92eb441eaf791469a49d2fe918f9
Skylar Lea <snl.coding@gmail.com>
2025-07-14 09:53:04 -0500

    next.config.ts changed to .js for cloud deployment

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+// This file is used to configure Next.js settings.
+// It can include options like enabling React strict mode, experimental features, etc.
+// For more information, visit: https://nextjs.org/docs/api-reference/next.config.js
+
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  experimental: {},
+};
+
+module.exports = nextConfig;
